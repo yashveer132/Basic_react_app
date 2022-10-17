9f0f61235cdaf9d8637177667813dbbf77995ed6
Yash <ys3792@srmist.edu.in>
2022-10-17 23:56:21 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+
+function App() {
+  return (
+    <Layout>
+      
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-403af-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(()=>{
+        navigate('/',{replace:true});
+    })
+  }
+  return (
+    <section>
+      <h1>ADD New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
